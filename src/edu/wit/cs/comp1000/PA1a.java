afd769c308f17d091f29bee7e3c0a2ca2aaea6b1
319535 <319535@664-161477-LAB.schools.mybps.org>
2022-10-31 10:38:58 -0400

    I submitted PA1 and PA2

--- src/edu/wit/cs/comp1000/PA1a.java
+++ src/edu/wit/cs/comp1000/PA1a.java
@@ -1,11 +1,32 @@
 package edu.wit.cs.comp1000;
 
+import java.util.Scanner;
+
 // TODO: document this class
 public class PA1a {
 
-	// TODO: document this function
+	
 	public static void main(String[] args) {
-		// TODO: write your code here
+			// TODO Auto-generated method stub
+	        Scanner input = new Scanner (System.in);
+	    int yards;
+	    int feet;
+	    int inches;
+	        
+	    System.out.printf("Enter The Value of Yards");
+	      yards = input.nextInt();
+	    
+	     System.out.printf("Enter the Value of Feet");
+	     feet = input.nextInt();
+	     
+	     System.out.printf("Enter the Value of Inches");
+	     inches = input.nextInt();
+	     
+	     int ans;
+	     ans = feet*12+yards*36+inches;
+	     System.out.print(ans);
+	
+	
 	}
 
 }
